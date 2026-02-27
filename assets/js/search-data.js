a98eeddc35127bb5f19a7116848525e7629ead1b
boxiaolzh <102209263+boxiaolzh@users.noreply.github.com>
2026-02-27 10:59:41 +0000

    Deploying to gh-pages from @ boxiaolzh/boxiaolzh.github.io@0d0d29605a35b6d5a692c5fe20c92147cfd30c8f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,8 +405,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-three-papers-accepted-to-dac-2026-ccf-a-thrilled-to-share-this-exciting-news",
-          title: '🎉 Three papers accepted to DAC 2026 (CCF-A)! Thrilled to share this exciting...',
+            },},{id: "news-two-papers-accepted-to-iccad-2025-ccf-b",
+          title: '🎉 Two papers accepted to ICCAD 2025 (CCF-B)!',
+          description: "",
+          section: "News",},{id: "news-three-papers-accepted-to-dac-2026-ccf-a",
+          title: '🎉 Three papers accepted to DAC 2026 (CCF-A)!',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
